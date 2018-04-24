87b01ef97d7771828e845f0c84ce7cb1243ed39f
Unknown <bronson_allgood@yahoo.com>
2018-04-24 15:31:11 -0600

    Player Script For Final Game

---
+++ PlayerMove.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+    public int playerSpeed = 10;
+    private bool facingRight = false;
+    public int playerJumpPower = 1250;
+    private float moveX;
+    public bool isGrounded;
+
+	// Update is called once per frame
+	void Update () {
+        PlayerMovement();
+        PlayerRaycast();
+	}
+
+    void PlayerMovement() {
+        //CONTROLS 
+        moveX = Input.GetAxis("Horizontal");
+        if (Input.GetButtonDown("Jump") && isGrounded == true)
+        {
+            Jump();
+        }
+        //ANIMATIONS
+        //PLAYER DIRECTION
+        if (moveX < 0.0f && facingRight == false) {
+            FlipPlayer();
+        }
+        else if (moveX > 0.0f && facingRight == true) {
+            FlipPlayer();
+        }
+        //PHYSICS
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2 (moveX * playerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+
+    }
+
+    void Jump() {
+        //JUMPING CODE
+        GetComponent<Rigidbody2D>().AddForce(Vector2.up * playerJumpPower);
+        isGrounded = false;
+
+    }
+
+    void FlipPlayer() {
+        facingRight = !facingRight;
+        Vector2 localScale = gameObject.transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        
+    }
+
+    void PlayerRaycast ()
+    {
+        //RAY UP
+        RaycastHit2D rayUp = Physics2D.Raycast(transform.position, Vector2.up);
+        if (rayUp != null &&  rayUp.collider != null && rayUp.distance < 0.9f && rayUp.collider.name == "Box2")
+        {
+            Destroy(rayUp.collider.gameObject);
+        }
+
+        //RAY DOWN
+        RaycastHit2D rayDown = Physics2D.Raycast(transform.position, Vector2.down);
+        if (rayDown != null &&  rayDown.collider != null && rayDown.distance < 0.9f && rayDown.collider.tag == "enemy")
+        {
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * 1000);
+            rayDown.collider.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.right * 200);
+            rayDown.collider.gameObject.GetComponent<Rigidbody2D>().gravityScale = 5;
+            rayDown.collider.gameObject.GetComponent<Rigidbody2D>().freezeRotation = false;
+            rayDown.collider.gameObject.GetComponent<BoxCollider2D>().enabled = false;
+            rayDown.collider.gameObject.GetComponent<EnemyMove>().enabled = false;
+        }
+        if (rayDown != null && rayDown.collider != null && rayDown.distance < 0.9f && rayDown.collider.tag != "enemy")
+        {
+            isGrounded = true;
+        }
+
+    }
+}
