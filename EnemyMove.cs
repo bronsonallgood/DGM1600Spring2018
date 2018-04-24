f93b7b3caede7e660855339497d6c94440e3e878
Unknown <bronson_allgood@yahoo.com>
2018-04-24 17:13:12 -0600

    trying to flip enemy when running into boxes

--- EnemyMove.cs
+++ EnemyMove.cs
@@ -2,29 +2,55 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyMove : MonoBehaviour {
+public class EnemyMove : MonoBehaviour
+{
 
     public int EnemySpeed;
     public int XMoveDirection;
+    private float moveX;
+    private bool facingRight = false;
 
 
     // Update is called once per frame
-    void Update () {
-        RaycastHit2D hit = Physics2D.Raycast (transform.position, new Vector2 (XMoveDirection, 0));
-        gameObject.GetComponent<Rigidbody2D> ().velocity = new Vector2 (XMoveDirection, 0) * EnemySpeed;
-        if (hit.distance < 0.7f) {
-            Flip ();
+    void Update()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(XMoveDirection, 0));
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(XMoveDirection, 0) * EnemySpeed;
+        if (hit.distance < 0.7f)
+        {
+            Flip();
             if (hit.collider.tag == "Player")
             {
                 Destroy(hit.collider.gameObject);
             }
         }
     }
-    void Flip() {
-        if (XMoveDirection > 0) {
+    void Flip()
+    {
+        if (XMoveDirection > 0)
+        {
             XMoveDirection = -1;
-        } else {
+        }
+        else
+        {
             XMoveDirection = 1;
         }
+
+        if (moveX < 0.0f && facingRight == false)
+        {
+            FlipPlayer();
+        }
+        else if (moveX > 0.0f && facingRight == true)
+        {
+            FlipPlayer();
+        }
+
+    }
+    void FlipPlayer()
+    {
+        facingRight = !facingRight;
+        Vector2 localScale = gameObject.transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
     }
 }
